7c52a6ff2b84d6e1b66af11afdef60d0fe0a4c60
Shredder <mashefor@gmail.com>
2019-11-05 23:56:06 +0100

    feat: add betslip component for mobile
    feat: add authguard for users not logged in route guarding

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(public authservice: AuthService, private router: Router) {
+
+  }
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.checkLoggedIn(state.url);
+  }
+
+  checkLoggedIn(url) {
+    if (this.authservice.isLoggedIn) {
+      return true;
+    } else {
+      this.authservice.redirectUrl = url;
+      this.router.navigate(['/auth/login']);
+      return false;
+    }
+  }
+}
---
+++ src/app/views/betslip/betslip.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+import { SportsService } from 'src/app/services/sports.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { AppComponent } from 'src/app/app.component';
+declare var swal: any;
+@Component({
+  selector: 'app-betslip',
+  templateUrl: './betslip.component.html',
+  styleUrls: ['./betslip.component.scss']
+})
+export class BetslipComponent implements OnInit {
+  betslip;
+
+  constructor(private dataservice: DataService,
+              private sportservice: SportsService, private authservice: AuthService, public appcomponent: AppComponent) { }
+
+  ngOnInit() {
+    this.dataservice.betSlip.subscribe(data => {
+      if (data) {
+        this.betslip = data;
+      }
+      const betslip = JSON.parse(localStorage.getItem('bordman-slip'));
+      if (betslip) {
+        this.betslip = betslip;
+      }
+    });
+  }
+
+  addToslip(slip) {
+    if (this.authservice.isLoggedIn) {
+      slip.createdAt = Date.now();
+      this.sportservice.addBets(slip).then(res => {
+        this.removeBet();
+      });
+    } else {
+      swal('You need to be signed in for that', {
+        icon: 'info',
+        buttons: {
+          cancel: true,
+          confirm: 'Login',
+        },
+      }).then(data => {
+        if (data) {
+          this.appcomponent.openThisModal();
+        }
+      });
+    }
+  }
+  removeBet() {
+    this.betslip = null;
+    localStorage.removeItem('bordman-slip');
+    this.dataservice.viewBetslip(null);
+  }
+
+}
